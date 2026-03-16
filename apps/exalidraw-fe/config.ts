90ca27a54ed7b569b1e5419a8d4d01fd0162bba3
Smart Coder <rafiaperween09@gmail.com>
2026-03-17 02:25:52 +0530

    debug-fix

--- apps/exalidraw-fe/config.ts
+++ apps/exalidraw-fe/config.ts
@@ -4,4 +4,4 @@ export const HTTP_BACKEND = process.env.NEXT_PUBLIC_HTTP_BACKEND;
 export const WS_URL = process.env.NEXT_PUBLIC_WS_URL;
 
 console.log(process.env.NEXT_PUBLIC_HTTP_BACKEND!);
-console.log(process.env.WS_URL!);
+console.log(process.env.NEXT_PUBLIC_WS_URL!);
