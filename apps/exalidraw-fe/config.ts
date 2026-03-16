e9215ee5f755c12ab1192dc90914074f4dd48a85
Smart Coder <rafiaperween09@gmail.com>
2026-03-17 02:23:33 +0530

    debug

--- apps/exalidraw-fe/config.ts
+++ apps/exalidraw-fe/config.ts
@@ -3,5 +3,5 @@ import 'dotenv/config';
 export const HTTP_BACKEND = process.env.NEXT_PUBLIC_HTTP_BACKEND;
 export const WS_URL = process.env.NEXT_PUBLIC_WS_URL;
 
-console.log(process.env.NEXT_PUBLIC_HTTP_BACKEND);
-console.log(process.env.WS_URL);
+console.log(process.env.NEXT_PUBLIC_HTTP_BACKEND!);
+console.log(process.env.WS_URL!);
